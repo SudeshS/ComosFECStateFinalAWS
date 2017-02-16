7eef351d07cf37955b35b83876c6e19f6451e3e9
SudeshS <sahu.k.sudesh@gmail.com>
2017-02-15 23:04:43 -0600

    echo test...

--- www/contact.php
+++ www/contact.php
@@ -5,6 +5,8 @@ $lastName = $_POST["lastname"];
 $email = $_POST["email"];
 $message = $_POST["message"];
 
+echo $firstName
+
 $db = pg_connect("host=ec2-54-235-248-197.compute-1.amazonaws.com port=5432 dbname=d2ecrfaqgg9aod user=eyyhoduuszkxsy password=27a645e9026d524ae7732ad46f824db68f7a83237b43db80a67adeae71b90241");
 $query = "INSERT INTO contactus VALUES ('$firstName', '$lastName', '$email', '$message')";
 $result = pg_query($query); 
